cb6bc8dd2cd03cde9a75100a650e3a8449ea5c4d
NineTailedFox687 <real_kawaii_cl4wz@proton.me>
2025-04-13 22:27:53 -0400

    Update chatbot_manager.py

--- chatbot_manager.py
+++ chatbot_manager.py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     AI: 3.14159
     Bob: Thanks
     '''
-    traits = ('load', 'annoying', 'talkative', 'rough')
+    traits = ()
 
     output = summerize_chat_messages(history)
     personality = generate_personality(traits)  
